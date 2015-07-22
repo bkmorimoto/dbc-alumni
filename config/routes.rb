253f0080f610c685a56a45bd475d3458be798342
Ben Morimoto <bkmorimoto@gmail.com>
2015-07-22 15:12:12 -0400

    added routes for users and sessions with some application helpers

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'projects#index'
 
   resources :projects
+  resources :users
+  resources :sessions
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
