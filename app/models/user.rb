7939071e2d54a1f092fa4b7177515b6c18861b38
Ben Morimoto <bkmorimoto@gmail.com>
2015-07-22 13:52:17 -0400

    add seed data for simple displays

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
   has_many :projects, foreign_key: "lead_id"
   has_many :project_users
   has_many :collaborations, through: :project_users, source: :project
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+20.times do
+  User.create(name: Faker::Internet.user_name, password: "password")
+end
+
+5.times do
+  lead_id = User.pluck(:id).sample
+  project = Project.create(name: Faker::Company.name, description: Faker::Lorem.paragraph, lead_id: lead_id)
+  4.times do
+    project.members << User.find(User.pluck(:id).sample)
+  end
+end
