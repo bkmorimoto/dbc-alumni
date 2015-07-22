8d5b982720250a4d1074bb241803d2cd59039658
Ben Morimoto <bkmorimoto@gmail.com>
2015-07-22 14:53:49 -0400

    create action for projects plus white-listing parameters

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,18 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    project = Project.new(project_params)
+    if project.save
+      redirect_to root_path
+    else
+      redirect_to new_project_path
+    end
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :lead_id)
+  end
+
 end
